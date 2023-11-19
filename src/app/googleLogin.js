dfb4305e3c40b618a118ca5b4d16727bd9740adf
aut43iamc@gmail.com <proyirvin2333@gmail.com>
2023-11-19 13:43:56 -0600

    No se que paso que mamo

--- src/app/googleLogin.js
+++ src/app/googleLogin.js
@@ -1,8 +1,8 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
 
-const GOOGLEAUTHBUTTON = document.querySelector('#Google-Auth')
+const googleAuthBotton = document.querySelector('#Google-Auth')
 
-GOOGLEAUTHBUTTON.addEventListener('click', async () => {
+googleAuthBotton.addEventListener('click', async () => {
     const provider = new GoogleAuthProvider();
 
     const auth = getAuth();
